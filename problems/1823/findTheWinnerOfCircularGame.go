c5f240139a98818c45bc5ef80667316721cd7ffd
lntvan166 <lntvan166@gmail.com>
2024-07-08 17:48:25 +0700

    Exercises: 1823

--- problems/1823/findTheWinnerOfCircularGame.go
+++ problems/1823/findTheWinnerOfCircularGame.go
@@ -1,17 +1,8 @@
+/*
+https://leetcode.com/problems/find-the-winner-of-the-circular-game/?envType=daily-question&envId=2024-07-08
+*/
 package exercises1823
 
-/*
-There are n friends that are playing a game. The friends are sitting in a circle and are numbered from 1 to n in clockwise order. More formally, moving clockwise from the ith friend brings you to the (i+1)th friend for 1 <= i < n, and moving clockwise from the nth friend brings you to the 1st friend.
-
-The rules of the game are as follows:
-
-1. Start at the 1st friend.
-2. Count the next k friends in the clockwise direction including the friend you started at. The counting wraps around the circle and may count some friends more than once.
-3. The last friend you counted leaves the circle and loses the game.
-4. If there is still more than one friend in the circle, go back to step 2 starting from the friend immediately clockwise of the friend who just lost and repeat.
-5. Else, the last friend in the circle wins the game.
-Given the number of friends, n, and an integer k, return the winner of the game.
-*/
 func FindTheWinner(n int, k int) int {
 	return findTheWinner(n, k)
 }
